86185ea5c32ad15a8405dab9858c5392d639c2a8
AluFu <0hp3261131m102u@ezweb.ne.jp>
2023-07-25 12:24:33 +0900

    [add]シークバー、再生速度の調整機能を追加

--- lib/view/full_screen_page.dart
+++ lib/view/full_screen_page.dart
@@ -11,24 +11,71 @@ class VideoPlayerFullScreen extends StatefulWidget {
 }
 
 class _VideoPlayerFullScreenState extends State<VideoPlayerFullScreen> {
+  late VideoPlayerController _videoController;
+  // シークバーの現在の値
+  late double _currentSliderValue;
+  // シークバーのドラッグ中フラグ
+  late bool _isDraggingSlider;
+  
   @override
   void initState() {
     super.initState();
-    widget.controller.setLooping(true);
-    widget.controller.play();
+    //ステータスバーを非表示にする
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    // フルスクリーンで動画を再生するために、VideoPlayerControllerを引き継ぐ
+    _videoController = widget.controller;
+    // ループ再生
+    _videoController.setLooping(true);
+    // 動画の読み込み
+    _currentSliderValue = 0.0;
+    // シークバーのドラッグ中かどうか
+    _isDraggingSlider = false;
+
+    // シークバーの更新
+    _videoController.addListener(() {
+      if (_videoController.value.isPlaying && !_isDraggingSlider) {
+        setState(() {
+          _currentSliderValue =
+              _videoController.value.position.inMilliseconds.toDouble();
+        });
+      }
+    });
   }
 
   @override
   void dispose() {
-    widget.controller.pause();
+    _videoController.dispose();
     super.dispose();
+  }
+
+  // シークバーのドラッグ開始時の処理
+  void _onSliderChangeStart(double value) {
+    setState(() {
+      _isDraggingSlider = true;
+    });
+  }
+
+  // シークバーの値が変更された時の処理
+  void _onSliderChanged(double value) {
+    setState(() {
+      _currentSliderValue = value;
+    });
+  }
+
+  // シークバーのドラッグ終了時の処理
+  void _onSliderChangeEnd(double value) {
+    setState(() {
+      _isDraggingSlider = false;
+      _videoController.seekTo(Duration(milliseconds: value.toInt()));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    backgroundColor: Color.fromRGBO(56, 56, 56, 1),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 102, 246, 97).withOpacity(0.5),
+        backgroundColor: Color.fromRGBO(56, 56, 56, 1),
         title: Text("戻る"),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
@@ -37,10 +84,81 @@ class _VideoPlayerFullScreenState extends State<VideoPlayerFullScreen> {
           },
         ),
       ),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: widget.controller.value.aspectRatio,
-          child: VideoPlayer(widget.controller),
+      body: 
+      Center(
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: _videoController.value.aspectRatio,
+              child: VideoPlayer(_videoController),
+            ),
+            // シークバー
+            SliderTheme(
+              data: SliderThemeData(
+                thumbColor: Colors.white, // つまみの色を白に変更
+                activeTrackColor: Colors.grey[400],   // 進んだ部分の色を灰色に変更 
+                inactiveTrackColor: Colors.grey[600], // 進んでいない部分の色を灰色に変更
+              ),
+              child: Slider(
+                value: _currentSliderValue,
+                min: 0.0,
+                max: _videoController.value.duration.inMilliseconds.toDouble(),
+                onChanged: _onSliderChanged,
+                onChangeStart: _onSliderChangeStart,
+                onChangeEnd: _onSliderChangeEnd,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // 動画の再生ボタン
+                IconButton(
+                  icon: Icon(Icons.play_arrow,color: Colors.white,size: 35),
+                  onPressed: () {
+                    _videoController.play();
+                  },
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                // 動画の一時停止ボタン
+                IconButton(
+                  icon: Icon(Icons.pause,color: Colors.white,size: 35),
+                  onPressed: () {
+                    _videoController.pause();
+                  },
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                // 動画の再生速度を変更するボタン
+                PopupMenuButton<double>(
+                  onSelected: (double value) {
+                    _videoController.setPlaybackSpeed(value);
+                  },
+                  icon: Icon(Icons.speed,color: Colors.white,size: 35),
+                  itemBuilder: (BuildContext context) => [
+                    PopupMenuItem<double>(
+                      value: 0.5,
+                      child: Text('0.5x'),
+                    ),
+                    PopupMenuItem<double>(
+                      value: 1.0,
+                      child: Text('1.0x'),
+                    ),
+                    PopupMenuItem<double>(
+                      value: 1.5,
+                      child: Text('1.5x'),
+                    ),
+                    PopupMenuItem<double>(
+                      value: 2.0,
+                      child: Text('2.0x'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
