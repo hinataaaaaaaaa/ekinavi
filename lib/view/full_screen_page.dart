cd69909d9fdc2a6022924ecd7e797701b223a518
清水 <noon3m.op.sh@gmail.com>
2023-07-25 12:46:51 +0900

    [add]フルスクリーン画面のついか

---
+++ lib/view/full_screen_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerFullScreen extends StatefulWidget {
+  final VideoPlayerController controller;
+  VideoPlayerFullScreen({required this.controller});
+
+  @override
+  _VideoPlayerFullScreenState createState() => _VideoPlayerFullScreenState();
+}
+
+class _VideoPlayerFullScreenState extends State<VideoPlayerFullScreen> {
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.setLooping(true);
+    widget.controller.play();
+  }
+
+  @override
+  void dispose() {
+    widget.controller.pause();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 102, 246, 97).withOpacity(0.5),
+        title: Text("戻る"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // フルスクリーンを終了して元の画面に戻る
+          },
+        ),
+      ),
+      body: Center(
+        child: AspectRatio(
+          aspectRatio: widget.controller.value.aspectRatio,
+          child: VideoPlayer(widget.controller),
+        ),
+      ),
+    );
+  }
+}
